0a4d49058513aa05f17494a103c0d5dfc544ba67
Mamun <ab9mamun@gmail.com>
2017-03-08 20:50:28 +0600

    Database codes are added

---
+++ build/generated/src/org/apache/jsp/index_jsp.java
@@ -0,0 +1,86 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+import muman.etc.Webpage;
+
+public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("<html>\n");
+      out.write("    <head>\n");
+      out.write("        <title>Welcome</title>\n");
+      out.write("        <meta charset=\"UTF-8\">\n");
+      out.write("        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n");
+      out.write("    </head>\n");
+      out.write("    <body>\n");
+      out.write("        ");
+
+            if(session.getAttribute("username")!=null) 
+                request.getRequestDispatcher(Webpage.home).forward(request, response);
+        
+      out.write("\n");
+      out.write("        \n");
+      out.write("       ");
+ out.print(Webpage.make(Webpage.signup, "Sign Up")); 
+      out.write("\n");
+      out.write("        <form method=\"post\" action=\"LoginProcess.do\">\n");
+      out.write("            <h1>Login</h1>\n");
+      out.write("            Enter your username: <input required type=\"text\" name=\"username\" /> <br/>\n");
+      out.write("            Enter your password: <input required type=\"password\" name=\"password\" /> <br/>\n");
+      out.write("            <input type=\"submit\" value=\"Login\" />\n");
+      out.write("        </form>\n");
+      out.write("               \n");
+      out.write("        \n");
+      out.write("    </body>\n");
+      out.write("</html>\n");
+      out.write("\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
